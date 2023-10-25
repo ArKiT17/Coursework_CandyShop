411e04fd706ad86ca1825a8bd5fd11998fbc4403
Макс Лик <maxlikarenko@gmail.com>
2023-10-25 23:22:23 +0200

    - додано хеш-сервіс для хешування паролів

---
+++ HasherPassword.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Coursework {
+	public static class HasherPassword {
+		public static string HashPassword(string password) {
+			using (SHA256 sha256 = SHA256.Create()) {
+				var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+				var hash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+				return hash;
+			}
+		}
+	}
+}
