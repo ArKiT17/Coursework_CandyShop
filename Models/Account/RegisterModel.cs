e1c6b6913121abf12b6879d2280fe1d2bbe90017
Макс Лик <maxlikarenko@gmail.com>
2023-10-26 20:36:36 +0200

    - змінене розположення футера - доданий файл стилю для форм реєстрації та авторизації - доданий файл стилю для авторизації та реєстрації - додані сторінки для реєстрації та авторизації - змінена майстер сторінка для відображення форм авторизації та реєстрації - додана обов'язкова пошта для реєстрації

--- Models/Account/RegisterModel.cs
+++ Models/Account/RegisterModel.cs
@@ -7,13 +7,17 @@ namespace Coursework.Models.Account
         [MinLength(5, ErrorMessage = "Логін має містити мінімум 5 символів")]
         public string Login { get; set; }
 
-		[DataType(DataType.Password)]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Введіть e-mail")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
 		[Required(ErrorMessage = "Введіть пароль")]
 		[MinLength(6, ErrorMessage = "Пароль має містити мінімум 6 символів")]
 		public string Password { get; set; }
 
 		[DataType(DataType.Password)]
-		[Required(ErrorMessage = "Підтвердіть пароль")]
+		[Required(ErrorMessage = "Повторіть пароль")]
 		[Compare("Password", ErrorMessage = "Паролі не співпадають")]
 		public string PasswordConfirm { get; set; }
 	}
