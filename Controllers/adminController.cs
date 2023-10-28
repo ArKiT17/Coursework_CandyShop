3020d2fb03d046f5853393c665715ca0e1510bb0
Макс Лик <maxlikarenko@gmail.com>
2023-10-28 23:48:22 +0200

    - додана перевірка на адміністратора для адмінпанелі - додані сторінки для адмінпанелі

--- Controllers/adminController.cs
+++ Controllers/adminController.cs
@@ -4,7 +4,36 @@ using WebApplication45.Models;
 namespace Coursework.Controllers {
 	public class AdminController : Controller {
 		public IActionResult Panel() {
+			isAdmin();
 			return View();
+		}
+
+		public async Task<IActionResult> Items() {
+			isAdmin();
+			return View();
+		}
+
+		public async Task<IActionResult> Staff() {
+			isAdmin();
+			return View();
+		}
+
+		public async Task<IActionResult> Users() {
+			isAdmin();
+			return View();
+		}
+
+		public async Task<IActionResult> News() {
+			isAdmin();
+			return View();
+		}
+
+		private bool isAdmin() {
+			if (!User.IsInRole("Admin")) {
+				RedirectToAction("Main", "Home");
+				return false;
+			}
+			return true;
 		}
 	}
 }
